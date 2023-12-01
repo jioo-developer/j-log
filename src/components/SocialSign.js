92a9e90c8ec4f0207bc4c1cfb0dedb451ebd5765
김지호 <jhkim022@lgupluspartners.co.kr>
2023-12-01 15:27:59 +0900

    소셜로그인 수정중

--- src/components/SocialSign.js
+++ src/components/SocialSign.js
@@ -11,17 +11,15 @@ function SocialSign({ authService, db }) {
         const newPassword = window.prompt(
           "소셜로그인에선 2차비밀번호를 설정하셔야합니다."
         );
-        result.user.updatePassword(newPassword).then(() => {
-          // db.collection("nickname")
-          //   .doc(result.user.displayName)
-          //   .set({ nickname: result.user.displayName });
-          // result.user.updateProfile({
-          //   photoURL: "./img/default.svg",
-          // });
+        result.user.updatePassword(newPassword);
+        db.collection("nickname")
+          .doc(result.user.displayName)
+          .set({ nickname: result.user.displayName });
+        result.user.updateProfile({
+          photoURL: "./img/default.svg",
         });
       }
     });
-
     // await 후 authService에서 받은 데이터 조회
   }
 
