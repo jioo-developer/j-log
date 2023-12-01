b38b431785dbedfdf26519fcd41321d71d49f3f6
김지호 <jhkim022@lgupluspartners.co.kr>
2023-12-01 11:21:34 +0900

    j-log 아이디 생성단 수정

--- src/components/SocialSign.js
+++ src/components/SocialSign.js
@@ -25,6 +25,12 @@ function SocialSign({ authService, db }) {
       // facebookData.displayName = result.user.displayName;
       // facebookData.profile = result.additionalUserInfo.profile.picture.data.url;
       //dispatch(함수(facebookData))
+      db.collection("nickname")
+        .doc(result.user.displayName)
+        .set({ nickname: result.user.displayName });
+      result.user.updateProfile({
+        photoURL: "./img/default.svg",
+      });
     });
     // await 후 authService에서 받은 데이터 조회
   }
